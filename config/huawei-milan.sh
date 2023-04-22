889b07a4b4c49063911bf85b02ad3344f3824cb2
Claris <118241780+Clrs17@users.noreply.github.com>
2023-04-22 22:25:50 +0800

    config: add config for Huawei Milan
    add config for Huawei Milan

---
+++ config/huawei-milan.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+OPTIONS=(-r vsn -r vsp)
+PANELS=(
+	[tianma_otm1906c_5p5_1080p_cmd]="huawei,tianma_otm1906c_5p5_1080p_cmd"
+	[tianma_fic8736_5p5_1080p_cmd]="huawei,tianma_fic8736_5p5_1080p_cmd"
+	[tianma_focal8716_5p5_1080p_video]="huawei,tianma_focal8716_5p5_1080p_video"
+	[jdi_r63452_5p5_1080p_cmd]="huawei,jdi_r63452_5p5_1080p_cmd"
+)
