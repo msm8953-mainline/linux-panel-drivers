e7c49961e116931926bea9897664d3a13e1c2632
白致远 <63627217+bzy-080408@users.noreply.github.com>
2023-07-07 20:22:46 +0800

    Update huawei-milan.sh

--- config/huawei-milan.sh
+++ config/huawei-milan.sh
@@ -2,8 +2,13 @@
 
 OPTIONS=(-r vsn -r vsp)
 PANELS=(
-	[tianma_otm1906c_5p5_1080p_cmd]="huawei,tianma_otm1906c_5p5_1080p_cmd"
-	[tianma_fic8736_5p5_1080p_cmd]="huawei,tianma_fic8736_5p5_1080p_cmd"
-	[tianma_focal8716_5p5_1080p_video]="huawei,tianma_focal8716_5p5_1080p_video"
-	[jdi_r63452_5p5_1080p_cmd]="huawei,jdi_r63452_5p5_1080p_cmd"
+	[boe_otm1906c_5p5_1080p_cmd]="huawei,milan-boe-otm1906c"
+	[boe_td4322_5p5_1080p_cmd]="huawei,milan-boe-td4322"
+	[boe_test1906c_5p5_1080p_cmd]="huawei,milan-boe-test1906c"
+	[ctc_nt35596s_5p5_1080p_video]="huawei,milan-ctc-nt35596s"
+	[ctc_otm1906c_5p5_1080p_cmd]="huawei,milan-ctc-otm1906c"
+	[jdi_r63452_5p5_1080p_cmd]="huawei,milan-jdi-r63452"
+	[tianma_otm1906c_5p5_1080p_cmd]="huawei,milan-tianma-otm1906c"
+	[tianma_fic8736_5p5_1080p_cmd]="huawei,milan-tianma-fic8736"
+	[tianma_focal8716_5p5_1080p_video]="huawei,milan-tianma-focal8716"
 )
