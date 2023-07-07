b549e4d0036cfc90f125772466f112d13a2172fe
白致远 <63627217+bzy-080408@users.noreply.github.com>
2023-07-07 20:22:46 +0800

    Update huawei-milan.sh

--- config/huawei-milan.sh
+++ config/huawei-milan.sh
@@ -2,8 +2,13 @@
 
 OPTIONS=(-r vsn -r vsp)
 PANELS=(
+	[boe_otm1906c_5p5_1080p_cmd]="huawei,boe_otm1906c_5p5_1080p_cmd"
+	[boe_td4322_5p5_1080p_cmd]="huawei,boe_td4322_5p5_1080p_cmd"
+	[boe_test1906c_5p5_1080p_cmd]="huawei,boe_test1906c_5p5_1080p_cmd"
+	[ctc_nt35596s_5p5_1080p_video]="huawei,ctc_nt35596s_5p5_1080p_video"
+	[ctc_otm1906c_5p5_1080p_cmd]="huawei,ctc_otm1906c_5p5_1080p_cmd"
+	[jdi_r63452_5p5_1080p_cmd]="huawei,jdi_r63452_5p5_1080p_cmd"
 	[tianma_otm1906c_5p5_1080p_cmd]="huawei,tianma_otm1906c_5p5_1080p_cmd"
 	[tianma_fic8736_5p5_1080p_cmd]="huawei,tianma_fic8736_5p5_1080p_cmd"
 	[tianma_focal8716_5p5_1080p_video]="huawei,tianma_focal8716_5p5_1080p_video"
-	[jdi_r63452_5p5_1080p_cmd]="huawei,jdi_r63452_5p5_1080p_cmd"
 )
