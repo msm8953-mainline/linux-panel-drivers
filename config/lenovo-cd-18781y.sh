fe88692f068a107a95501b6289f064c547ac7c4b
Felix Kaechele <felix@kaechele.ca>
2024-02-16 11:10:17 -0500

    config/dtb: Add Lenovo ThinkSmart View (CD-18781Y)
    Signed-off-by: Felix Kaechele <felix@kaechele.ca>

---
+++ config/lenovo-cd-18781y.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+OPTIONS=(-r vsn -r vsp)
+PANELS=(
+	[boyift8201_800p_video]="lenovo,cd-18781y-ft8201"
+	[hx83100a_800p_video]="lenovo,cd-18781y-hx83100a"
+	[starry_800p_video]="lenovo,cd-18781y-jd9365"
+)
