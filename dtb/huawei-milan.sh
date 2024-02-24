bb3df0c4f12f002a5c14ffd56698b9f67385059e
bzy-080408 <Bzy080408@outlook.com>
2024-02-24 19:22:12 +0800

    config/dtb: Add Huawei Huawei Maimang 5 / G9 (Plus) / Nova (Plus) (milan)
    Signed-off-by: bzy-080408 <Bzy080408@outlook.com>

---
+++ dtb/huawei-milan.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+OPTIONS=(-r vsn -r vsp)
+PANELS=(
+	[milan-boe-otm1906c]="huawei,boe_otm1906c_5p5_1080p_cmd"
+	[milan-boe-td4322]="huawei,boe_td4322_5p5_1080p_cmd"
+	[milan-boe-test1906c]="huawei,boe_test1906c_5p5_1080p_cmd"
+	[milan-ctc-nt35596s]="huawei,ctc_nt35596s_5p5_1080p_video"
+	[milan-ctc-otm1906c]="huawei,ctc_otm1906c_5p5_1080p_cmd"
+	[milan-jdi-r63452]="huawei,jdi_r63452_5p5_1080p_cmd"
+	[milan-tianma_otm1906c]="huawei,tianma_otm1906c_5p5_1080p_cmd"
+	[milan-tianma-fic8736]="huawei,tianma_fic8736_5p5_1080p_cmd"
+	[milan-tianma-focal8716]="huawei,tianma_focal8716_5p5_1080p_video"
+)
