0030b1b069678f72ac4d692570a0438cf2fbbea0
Andrea Bolognani <eof@kiyuko.org>
2024-03-09 18:54:51 +0100

    xiaomi-daisy: Generate otm1911_fhdplus_video driver too
    Signed-off-by: Andrea Bolognani <eof@kiyuko.org>

--- config/xiaomi-daisy.sh
+++ config/xiaomi-daisy.sh
@@ -4,5 +4,6 @@
 OPTIONS=(-r vsn -r vsp)
 PANELS=(
 	[ili7807_fhdplus_video]="mdss,ili7807-fhdplus"
+	[otm1911_fhdplus_video]="mdss,otm1911-fhdplus"
 	[hx8399c_fhdplus_video]="himax,hx8399c-fhdplus"
 )
