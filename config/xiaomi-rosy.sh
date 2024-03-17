63fedfff3b1fb7f3bcbcbc06677fc207322cc190
Barnabás Czémán <trabarni@gmail.com>
2024-03-17 18:12:05 +0100

    config/dtb: Add Xiaomi Redmi 5 (rosy)

---
+++ config/xiaomi-rosy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+
+OPTIONS=(-r vsn -r vsp)
+PANELS=(
+	[ft8613_ebbg_5p7_720p_video]="xiaomi,rosy-ft8613-ebbg"
+	[ft8613_csot_5p7_720p_video]="xiaomi,rosy-ft8613-csot"
+	[ft8006m_boe_5p7_720p_video]="xiaomi,rosy-ft8006m-boe"
+)
