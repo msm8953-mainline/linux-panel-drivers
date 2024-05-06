bda5971caee51e75ef5d0df4ae6893535ca53446
Barnabás Czémán <trabarni@gmail.com>
2024-05-06 22:15:59 +0200

    config/dtb: Update Motorola Moto G6 (ali) config

--- config/motorola-ali.sh
+++ config/motorola-ali.sh
@@ -3,5 +3,7 @@
 
 OPTIONS=(-r vsn -r vsp)
 PANELS=(
-	[tianma_565_1080p_vid_v0]="tianma,tl057fvxp01"
+	[boe_565_1080p_vid_v0]="motorola,ali-panel-boe"
+	[tianma_565_1080p_vid_v0]="motorola,ali-panel-tianma"
+	# [auo_565_1080p_vid_v0]="motorola,ali-panel-auo"
 )
