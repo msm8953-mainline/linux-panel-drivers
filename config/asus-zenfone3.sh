f65d2b89aad7c2534a278ca8006933af7401f00f
Antony Kurniawan <Smankusors@users.noreply.github.com>
2024-08-25 18:52:50 +0000

    config/dtb: Add Asus Zenfone 3 (zenfone3)

---
+++ config/asus-zenfone3.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+OPTIONS=(-r vsn -r vsp)
+PANELS=(
+	[tm5p2_r63350_1080p_video]="asus,ze520kl-r63350-tm"
+	[boe5p2_ili7807b_1080p_video]="asus,ze520kl-ili7807b-boe"
+	[ctc5p5_ili7807b_1080p_video]="asus,ze552kl-ili7807b-ctc"
+	[tm5p5_r63350_1080p_video]="asus,ze552kl-r63350-tm"
+	[txd5p5_nt35596_1080p_video]="asus,ze552kl-nt35596-txd"
+	[lce5p5_otm1901a_1080p_video]="asus,ze552kl-otm1901a-lce"
+)
