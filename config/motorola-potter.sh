d6a26cf67097be4bc4eacb82c6b0c34b63ef703a
Barnabás Czémán <trabarni@gmail.com>
2024-09-27 21:20:55 +0200

    config: motorola-potter: add dumb-dcs
    Add dumb-dcs paramter as a possible fix for very dimmed display.

--- config/motorola-potter.sh
+++ config/motorola-potter.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-OPTIONS=(-r vsn -r vsp)
+OPTIONS=(-r vsn -r vsp --dumb-dcs)
 PANELS=(
 	[tianma_520_1080p_vid_v0]="tianma,tl052vdxp02"
 	[boe_520_1080p_vid_v0]="boe,bs052fhm-a00-6c01"
